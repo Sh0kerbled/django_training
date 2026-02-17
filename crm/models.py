6da0a59137e0ec3954e084cd84b0b2ca2347c2f9
Dauren <d.k.nurgaziev@gmail.com>
2026-02-17 12:23:20 +0500

    registred new admin

--- crm/models.py
+++ crm/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Order(models.Model):
     order_dt = models.DateTimeField(auto_now=True)
-    order_name = models.CharField(max_length=200)
-    order_phone = models.CharField(max_length=200)
+    order_name = models.CharField(max_length=200, verbose_name="Имя")
+    order_phone = models.CharField(max_length=200, verbose_name="Номер")
