45513c7389051e85ada64f3ab9ee7af0c7dc4d2a
Dauren <d.k.nurgaziev@gmail.com>
2026-02-17 23:07:13 +0500

    mini update

--- crm/models.py
+++ crm/models.py
@@ -4,3 +4,4 @@ class Order(models.Model):
     order_dt = models.DateTimeField(auto_now=True)
     order_name = models.CharField(max_length=200, verbose_name="Имя")
     order_phone = models.CharField(max_length=200, verbose_name="Номер")
+    order_email - models.CharField(max_length=50, verbose_name="Email")
